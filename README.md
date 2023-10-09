124aa8518240a6a5edd3446c4e7540739f0bd4ad
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-10-09 19:11:25 -0300

    Add instructions

---
+++ README.md
@@ -0,0 +1,18 @@
+Ao clonar o repositório, execute os comandos:
+
+```
+npm run venv
+```
+
+```
+source venv/bin/activate
+```
+```
+npm run migrate
+```
+
+Para executar o servidor, rode:
+
+```
+npm start
+```
--- cheatsheet.sh
+++ cheatsheet.sh
@@ -2,8 +2,6 @@ python manage.py runserver  # runs server (i.e. npm run dev)
 
 # a Django project may have 1 or more apps
 python manage.py startapp notes  # creates new app called "notes"
-
 python manage.py makemigrations  # generates a script that will create the new tables when executed
 python manage.py migrate         # executes the script
-
 python manage.py createsuperuser
